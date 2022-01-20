a0193ee02eb6653dae1d8ac35e27100082e132c0
Gregor <apriljunge@gmail.com>
2022-01-20 12:50:51 +0100

    Extend Road methods

--- src/trafficsim/model/grid/Road.java
+++ src/trafficsim/model/grid/Road.java
@@ -75,4 +75,111 @@ public class Road {
     public Vehicle getVehicle(int position) {
         return this.road[position];
     }
+
+    public void applyRules() {
+        for (int position = 0; position < this.road.length; position++) {
+            Vehicle v = this.road[position];
+
+            if (v == null) {
+                continue;
+            }
+
+            accelerate(v);
+            brake(v, position);
+            dawdle(v);
+            move(v, position);
+        }
+    }
+
+    public boolean isEmpty(int lower, int upper) {
+        int from = Math.max(0, lower);
+        int to = Math.min(upper, this.road.length);
+
+        for (int i = from; i < to; i++) {
+            if (this.road[i] != null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int getDistanceToNextCar(int position) {
+        for (int i = position; i < this.road.length; i++) {
+            if (this.road[i] != null) {
+                return i - position - (this.road[i].getLength() + (int)Math.ceil((double)this.maxV / 2));
+            }
+        }
+
+        return this.maxV + 1;
+    }
+
+    private void accelerate(Vehicle v) {
+        int speed = v.getSpeed();
+
+        if (speed < this.maxV) {
+            v.setSpeed(speed + 1);
+        }
+    }
+
+    private void brake(Vehicle v, int position) {
+        int distanceToNextCar = getDistanceToNextCar(position);
+        int distanceToEnd = distanceToEnd(position);
+
+        if (distanceToNextCar <= v.getSpeed()) {
+            v.setSpeed(distanceToNextCar - 1);
+        }
+
+        if (distanceToEnd < 2 * this.maxV && v.getSpeed() > 1) {
+            v.setSpeed(v.getSpeed() - 1);
+        }
+    }
+
+    private void dawdle(Vehicle v) {
+        int speed = v.getSpeed();
+
+        if (isDawdling()) {
+            if (speed > 0) {
+                v.setSpeed(speed + 1);
+            }
+        }
+    }
+
+    private void move(Vehicle v, int position) {
+        if (v.getSpeed() <= 0) {
+            return;
+        }
+
+        if (isFirst(v) && distanceToEnd(position) < v.getSpeed()) {
+            transfer(v, position);
+        } else {
+            moveForward(v, position);
+        }
+    }
+
+    private boolean isFirst(Vehicle v) {
+        for (Vehicle vehicle:this.road) {
+            if (vehicle != null) {
+                return vehicle == v;
+            }
+        }
+
+        return false;
+    }
+
+    private int distanceToEnd(int position) {
+        return this.road.length - position;
+    }
+
+    private boolean isDawdling() {
+        return rnd.nextDouble() < this.dawdleFactor;
+    }
+
+    private void transfer(Vehicle v, int position) {
+        // TODO
+    }
+
+    private void moveForward(Vehicle v, int position) {
+        // TODO
+    }
 }
