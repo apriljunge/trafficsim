84bf2cc4b62c7a710796362403cc8c5977951020
Gregor <apriljunge@gmail.com>
2022-01-20 12:50:05 +0100

    Implement TrafficNode

--- src/trafficsim/model/grid/TrafficNode.java
+++ src/trafficsim/model/grid/TrafficNode.java
@@ -1,4 +1,8 @@
 package trafficsim.model.grid;
 
+import trafficsim.model.traffic.Vehicle;
+
 public interface TrafficNode {
+    public boolean canMoveTo(int position);
+    public void transferVehicle(Road origin, Vehicle v, int position);
 }
